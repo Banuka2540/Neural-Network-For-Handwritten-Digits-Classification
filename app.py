b7fc5035264023a36005b3e9ea7dee43aa4e97a1
yuthila_banuka <banukadeseram12@gmail.com>
2026-02-03 11:07:54 +0530

    index and app.py errors handling

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
 from flask import Flask , render_template , request
-import pickle
+import pickle 
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
-def predict(number):
-    filename = 'Model/model.pickle'
-    with open(filename,'rb') as file :
-        model = pickle.load(file)
-    pred_number = model.predict([number])   
-    return pred_number
 
+filename = 'Model/model.pickle'
+with open(filename,'rb') as file :
+    model = pickle.load(file)
+
+def predict(image):
+    pred_image = model.predict(image)   
+    return pred_image
+
+def preprocessing(image):
+        new_image  = Image.open(image).convert("L")
+        new_image  = new_image.resize((28 , 28))
+        new_image  = np.array(new_image)
+        new_image  = new_image / 255
+        new_image  = new_image.reshape(1,28*28)
+        return new_image
 
 
 @app.route("/",methods=['GET','POST'])
 def Home():
     pred = 0 
     if request.method == 'POST':
-        num = request.get_button["btn-1"]   
-        return render_template("index.html",pred_number = pred )
+        if 'input-image' not in request.files:
+            return render_template("index.html", pred_number="No file uploaded") 
+        else :
+            image = request.files['input-image']
+        image = preprocessing(image)
+        pred = predict(image)
+        pred = int(pred[0])
+
+    return render_template("index.html",pred_number = pred )
 
 if __name__ == '__main__' :
     app.run()
