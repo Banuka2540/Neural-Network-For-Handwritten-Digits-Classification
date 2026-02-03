6b6dcb403c0661f2939bdf03bd1e36228f9001a7
yuthila_banuka <banukadeseram12@gmail.com>
2026-02-03 09:00:32 +0530

    Modified the app.py and some html

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
+import pickle
 
 app = Flask(__name__)
 
-@app.route("/")
+def predict(number):
+    filename = 'Model/model.pickle'
+    with open(filename,'rb') as file :
+        model = pickle.load(file)
+    pred_number = model.predict([number])   
+    return pred_number
+
+
+
+@app.route("/",methods=['GET','POST'])
 def Home():
-    return render_template("index.html")
+    pred = 0 
+    if request.method == 'POST':
+        num = request.get_button["btn-1"]   
+        return render_template("index.html",pred_number = pred )
 
 if __name__ == '__main__' :
     app.run()
