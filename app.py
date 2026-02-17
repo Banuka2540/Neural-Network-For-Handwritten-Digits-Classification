d389207815f4ee30c422ea67d5d858860d555cff
yuthila_banuka <banukadeseram12@gmail.com>
2026-02-17 22:58:29 +0530

    recreated the app and html , css

--- app.py
+++ app.py
@@ -1,41 +1,43 @@
-from flask import Flask , render_template , request
-import pickle 
-import numpy as np
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
 from PIL import Image
+import numpy as np
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
-filename = 'Model/model.pickle'
-with open(filename,'rb') as file :
-    model = pickle.load(file)
+model = load_model('model/mnist_model.h5')  # Load Keras model once
 
-def predict(image):
-    pred_image = model.predict(image)   
-    return pred_image
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-def preprocessing(image):
-        new_image  = Image.open(image).convert("L")
-        new_image  = new_image.resize((28 , 28))
-        new_image  = np.array(new_image)
-        new_image  = new_image / 255
-        new_image  = new_image.reshape(1,28*28)
-        return new_image
+def predict_digit(filepath):
+    img = Image.open(filepath).convert('L')
+    img = img.resize((28,28))
+    img_array = np.array(img).reshape(1,28,28,1)
+    img_array = img_array / 255.0
+    pred = model.predict(img_array)
+    return np.argmax(pred, axis=1)[0]
 
-
-@app.route("/",methods=['GET','POST'])
-def Home():
-    pred = 0 
+@app.route('/', methods=['GET','POST'])
+def home():
+    prediction = None
     if request.method == 'POST':
-        if 'input-image' not in request.files:
-            return render_template("index.html", pred_number="No file uploaded") 
-        else :
-            image = request.files['input-image']
-        image = preprocessing(image)
-        pred = predict(image)
-        pred = int(pred[0])
+        if 'digit_image' not in request.files:
+            return "No file uploaded"
+        file = request.files['digit_image']
+        if file.filename == '':
+            return "No file selected"
+        if file and allowed_file(file.filename):
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            prediction = predict_digit(filepath)
+    return render_template('index.html', prediction=prediction)
 
-    return render_template("index.html",pred_number = pred )
-
-if __name__ == '__main__' :
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
