9f6014b798d3899ad98658e0b7d596006d05c5b1
yuthila_banuka <banukadeseram12@gmail.com>
2026-02-21 09:58:11 +0530

    recreated the model and UI

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask , render_template , request
-
+from tensorflow.keras.models import load_model
+import numpy as np 
+from PIL import Image , ImageOps
 
 app = Flask(__name__)
+model = load_model(r"D:\python_projects\DL\Neural Network For Handwritten Digits Classification\model\model.h5")
 
+def preprocessing(image):
+    new_image  = Image.open(image).convert("L")
+    new_image  = new_image.resize((28 , 28))
+    new_image  = np.array(new_image)
+    new_image  = new_image / 255
+    new_image  = new_image.reshape(1,28*28)
+    return new_image
 
-def pred(img):
-    from tensorflow.keras.models import load_model
-    model = load_model(r"D:\python_projects\DL\Neural Network For Handwritten Digits Classification\model\model.h5")
-    pred_value = model.predict(img)
-    return pred_value
+def predict(new_image):
+    prediction = model.predict(new_image)
+    max_pred = np.argmax(prediction)
+    return max_pred
 
 @app.route('/' , methods = ['GET','POST'])
 def home():
     prediction = 0
     if request.method =='POST':
         image = request.files['image']
-        prediction = pred(image)
+        pre_image = preprocessing(image)
+        prediction = predict(pre_image)
 
     return render_template("index.html",pred_value = prediction)
 
